afe0e14e31c0aa23d84ce77c8727333be1c69d74
Apurva <apurva.gurme@thoughtworks.com>
2019-12-13 14:19:57 +0530

    Added chai to remove conditional symbol from assert.

--- test/testLine.js
+++ test/testLine.js
@@ -1,5 +1,5 @@
 const Line = require('../src/line');
-const assert = require('assert');
+const { assert } = require('chai');
 
 describe('Line', function() {
   describe('toString', function() {
@@ -19,12 +19,12 @@ describe('Line', function() {
     it('isEqualTo method should give false when one single point of a line is different', function() {
       const line1 = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
       const line2 = new Line({ x: 1, y: 2 }, { x: 3, y: 5 });
-      assert.ok(!line1.isEqualTo(line2));
+      assert.isNotOk(line1.isEqualTo(line2));
     });
     it('isEqualTo method should give false when the other line is not instance of the same Line class', function() {
       const line1 = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
       const line2 = 'not a line';
-      assert.ok(!line1.isEqualTo(line2));
+      assert.isNotOk(line1.isEqualTo(line2));
     });
   });
 });
