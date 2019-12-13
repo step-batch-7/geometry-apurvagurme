8513de4c521813d730878b471e33bcbaede086a9
Apurva <apurva.gurme@thoughtworks.com>
2019-12-13 09:54:05 +0530

    working for both toString and isEqualTo method
    What changes you have made?
    changed the structure of the class.
    Added toString method.
    
    Why this change is necessary?
    for a proper sturcture of lines we need endpoints.
    we can see our class structure.
    
    What is the impact of the change?
    more features and proper structure.

--- src/line.js
+++ src/line.js
@@ -1,15 +1,25 @@
+const arePointsEqual = function(current, other) {
+  return (
+    current.endA.x == other.endA.x &&
+    current.endA.y == other.endA.y &&
+    current.endB.y == other.endB.y &&
+    current.endB.x == other.endB.x
+  );
+};
+
 class Line {
-  constructor(x, y, l) {
-    this.xCoordinate = x;
-    this.yCoordinate = y;
-    this.lengthOfLine = l;
+  constructor(x1, y1, x2, y2) {
+    this.endA = { x: x1, y: y1 };
+    this.endB = { x: x2, y: y2 };
   }
-  get toString() {
-    return 'Line { xCoordinate: 1, yCoordinate: 2, lengthOfLine: 3 }';
+
+  toString() {
+    return `Line (${this.endA.x},${this.endA.y})-----(${this.endB.x},${this.endB.y})`;
   }
+
   isEqualTo(other) {
-    return other.lengthOfLine == this.lengthOfLine;
+    return arePointsEqual(this, other);
   }
 }
 
-module.exports = Line;
+module.exports = { Line, arePointsEqual };
--- test/testLine.js
+++ test/testLine.js
@@ -1,19 +1,39 @@
-const Line = require('../src/line');
+const { Line, arePointsEqual } = require('../src/line');
 const assert = require('assert');
 
 describe('Line', function() {
-  it('toString method should give the string representation of line object', function() {
-    const a = new Line(2, 4, 2);
-    let actual = a.toString;
-    let expected = 'Line { xCoordinate: 1, yCoordinate: 2, lengthOfLine: 3 }';
-    assert.strictEqual(actual, expected);
+  describe('toString', function() {
+    it('toString method should give the string representation of line object', function() {
+      const a = new Line(1, 1, 4, 3);
+      let expected = 'Line (1,1)-----(4,3)';
+      let actual = a.toString();
+      assert.strictEqual(actual, expected);
+    });
   });
+  describe('isEqualTo', function() {
+    it('isEqualTo method should give whether a line is equal to another line', function() {
+      const a = new Line(1, 1, 4, 3);
+      const b = new Line(1, 1, 4, 3);
+      let actual = a.isEqualTo(b);
+      let expected = true;
+      assert.strictEqual(actual, expected);
+    });
+    it('isEqualTo method should give false when one single point of a line is different', function() {
+      const a = new Line(1, 2, 3, 4);
+      const b = new Line(1, 2, 3, 5);
+      let actual = a.isEqualTo(b);
+      let expected = false;
+      assert.strictEqual(actual, expected);
+    });
+  });
+});
 
-  it('isEqualTo method should give whether a line is equal to another line', function() {
-    const a = new Line(2, 4, 2);
-    const b = new Line(4, 6, 2);
-    let actual = a.isEqualTo(b);
-    let expected = true;
+describe('arePointsEqual', function() {
+  it('should give true value if every points are equal', function() {
+    const current = { endA: { x: 1, y: 2 }, endB: { x: 3, y: 4 } };
+    const other = { endA: { x: 1, y: 2 }, endB: { x: 3, y: 4 } };
+    const expected = true;
+    const actual = arePointsEqual(current, other);
     assert.strictEqual(actual, expected);
   });
 });
